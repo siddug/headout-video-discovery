8926a7c9fc774ede291d1d19c6164427198fb1dd
Siddhartha <siddhartha.gunti191@gmail.com>
2017-02-18 06:55:17 +0530

    forgot to add model changes

--- headout/headout/Models/VideoPlayer.swift
+++ headout/headout/Models/VideoPlayer.swift
@@ -8,27 +8,45 @@
 
 import Foundation
 
+class HeadoutVideo {
+    var videoUrl: String
+    var linkUrl: String
+    
+    init(videoUrl: String, linkUrl: String) {
+        self.videoUrl = videoUrl
+        self.linkUrl = linkUrl
+    }
+}
+
 class VideoPlayer {
     static let shared = VideoPlayer()
     
     fileprivate init() { }
+    
     let urlArray = [
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487353601/videoplayback_amdsji.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/v1487373441/videoplayback_ui4yho.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374531/videoplayback_obylet.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_780/v1487374700/videoplayback_e3zvox.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374852/videoplayback_rvhcga.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,g_center,h_1280,w_720/v1487375084/videoplayback_nsrxka.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375265/videoplayback_naggi8.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375655/videoplayback_tsrez2.mp4",
-        "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375818/videoplayback_awa49f.mp4"
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487353601/videoplayback_amdsji.mp4", linkUrl: "https://www.headout.com/tour/512/united-states/new-york/wicked"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487373441/videoplayback_ui4yho.mp4", linkUrl: "https://www.headout.com/tour/515/united-states/new-york/matilda-the-musical"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374531/videoplayback_obylet.mp4", linkUrl: "https://www.headout.com/tour/517/united-states/new-york/the-phantom-of-the-opera"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_780/v1487374700/videoplayback_e3zvox.mp4", linkUrl: ""),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374852/videoplayback_rvhcga.mp4", linkUrl: ""),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,g_center,h_1280,w_720/v1487375084/videoplayback_nsrxka.mp4", linkUrl: ""),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375265/videoplayback_naggi8.mp4", linkUrl: ""),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375655/videoplayback_tsrez2.mp4", linkUrl: ""),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375818/videoplayback_awa49f.mp4", linkUrl: "")
     ]
     
     var playPosition = 0
     
-    func getURLString() -> String? {
+    func getVideoUrl() -> String? {
         if playPosition < urlArray.count {
-            return urlArray[playPosition]
+            return urlArray[playPosition].videoUrl
+        }
+        return nil
+    }
+    
+    func getLinkUrl() -> String? {
+        if playPosition < urlArray.count {
+            return urlArray[playPosition].linkUrl
         }
         return nil
     }
