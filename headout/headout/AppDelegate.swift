76fbfc09e1d259b0dde2c36d6b7a6aafb2c52380
Siddhartha <siddhartha.gunti191@gmail.com>
2017-02-18 08:20:46 +0530

    nit fixes

--- headout/headout/AppDelegate.swift
+++ headout/headout/AppDelegate.swift
@@ -19,8 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
-        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
-        GIDSignIn.sharedInstance().delegate = self
+//        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
+//        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -46,12 +46,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
-        -> Bool {
+//    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+//        -> Bool {
 //            return GIDSignIn.sharedInstance().handle(url,
 //                                                     sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
 //                                                     annotation: [:])
-    }
+            
+            
+    //}
     
 //    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
 //        // ...
