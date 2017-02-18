35daa23d44fd1b7a44803a5f872ae09b4ff24300
Varun Jindal <varunjindal9@gmail.com>
2017-02-18 17:42:17 +0530

    add all video. absorb all touches in no bg view.

--- headout/headout/Models/VideoPlayer.swift
+++ headout/headout/Models/VideoPlayer.swift
@@ -27,18 +27,18 @@ class VideoPlayer {
     }
 
     let allUrlArray = [
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487353601/videoplayback_amdsji.mp4", linkUrl: "https://www.headout.com/tour/512/united-states/new-york/wicked"),
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487373441/videoplayback_ui4yho.mp4", linkUrl: "https://www.headout.com/tour/515/united-states/new-york/matilda-the-musical"),
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374531/videoplayback_obylet.mp4", linkUrl: "https://www.headout.com/tour/517/united-states/new-york/the-phantom-of-the-opera"),
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_780/v1487374700/videoplayback_e3zvox.mp4", linkUrl: "https://www.headout.com/tour/891"), // LV video
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374852/videoplayback_rvhcga.mp4", linkUrl: "https://www.headout.com/tour/5331"), // LDN video
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,g_center,h_1280,w_720/v1487375084/videoplayback_nsrxka.mp4", linkUrl: "https://www.headout.com/tour/4548/united-states/new-york/the-glass-menagerie"),
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375265/videoplayback_naggi8.mp4", linkUrl: "https://www.headout.com/tour/519/united-states/new-york/chicago"),
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375655/videoplayback_tsrez2.mp4", linkUrl: "https://www.headout.com/tour/765/united-states/las-vegas/michael-jackson-one-by-cirque-du-soleil?stage=content"), // LV video
-//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375818/videoplayback_awa49f.mp4", linkUrl: "https://www.headout.com/tour/892/united-states/las-vegas/k-by-cirque-du-soleil") // LV video
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487353601/videoplayback_amdsji.mp4", linkUrl: "https://www.headout.com/tour/512/united-states/new-york/wicked"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487373441/videoplayback_ui4yho.mp4", linkUrl: "https://www.headout.com/tour/515/united-states/new-york/matilda-the-musical"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374531/videoplayback_obylet.mp4", linkUrl: "https://www.headout.com/tour/517/united-states/new-york/the-phantom-of-the-opera"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_780/v1487374700/videoplayback_e3zvox.mp4", linkUrl: "https://www.headout.com/tour/891"), // LV video
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487374852/videoplayback_rvhcga.mp4", linkUrl: "https://www.headout.com/tour/5331"), // LDN video
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,g_center,h_1280,w_720/v1487375084/videoplayback_nsrxka.mp4", linkUrl: "https://www.headout.com/tour/4548/united-states/new-york/the-glass-menagerie"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375265/videoplayback_naggi8.mp4", linkUrl: "https://www.headout.com/tour/519/united-states/new-york/chicago"),
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375655/videoplayback_tsrez2.mp4", linkUrl: "https://www.headout.com/tour/765/united-states/las-vegas/michael-jackson-one-by-cirque-du-soleil?stage=content"), // LV video
+        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/c_fill,h_1280,w_720/v1487375818/videoplayback_awa49f.mp4", linkUrl: "https://www.headout.com/tour/892/united-states/las-vegas/k-by-cirque-du-soleil"), // LV video
         // sample videos
-        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/v1487383842/big_buck_bunny_720p_1mb_gefmrk.mp4", linkUrl: "https://www.headout.com"),
-        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/v1487383842/big_buck_bunny_720p_1mb_gefmrk.mp4", linkUrl: "https://www.headout.com"),
+//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/v1487383842/big_buck_bunny_720p_1mb_gefmrk.mp4", linkUrl: "https://www.headout.com"),
+//        HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/v1487383842/big_buck_bunny_720p_1mb_gefmrk.mp4", linkUrl: "https://www.headout.com"),
         HeadoutVideo(videoUrl: "http://res.cloudinary.com/dscs5qleu/video/upload/v1487383842/big_buck_bunny_720p_1mb_gefmrk.mp4", linkUrl: "https://www.headout.com")
     ]
     
